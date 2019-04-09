6baf028a9fdb03960a7b842b9c302f38fd9ae268
howon <howone1sky@gmail.com>
2019-04-09 15:18:42 +0300

    StringField-FloatField

--- app/forms.py
+++ app/forms.py
@@ -1,24 +1,24 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, FloatField
 from wtforms.validators import DataRequired
 
 
 class Kurav(Form):
-    a = StringField(validators=[DataRequired()])
-    b = StringField(validators=[DataRequired()])
-    c = StringField(validators=[DataRequired()])
+    a = FloatField(validators=[DataRequired()])
+    b = FloatField(validators=[DataRequired()])
+    c = FloatField(validators=[DataRequired()])
 
 
 class Treagle_on_ploskost(Form):
-    ax = StringField(validators=[DataRequired()])
-    ay = StringField(validators=[DataRequired()])
-    bx = StringField(validators=[DataRequired()])
-    by = StringField(validators=[DataRequired()])
-    cx = StringField(validators=[DataRequired()])
-    cy = StringField(validators=[DataRequired()])
+    ax = FloatField(validators=[DataRequired()])
+    ay = FloatField(validators=[DataRequired()])
+    bx = FloatField(validators=[DataRequired()])
+    by = FloatField(validators=[DataRequired()])
+    cx = FloatField(validators=[DataRequired()])
+    cy = FloatField(validators=[DataRequired()])
 
 class Trapecia(Form):
-    osn1 = StringField(validators=[DataRequired()])
-    osn2 = StringField(validators=[DataRequired()])
-    reb1 = StringField(validators=[DataRequired()])
-    reb2 = StringField(validators=[DataRequired()])
+    osn1 = FloatField(validators=[DataRequired()])
+    osn2 = FloatField(validators=[DataRequired()])
+    reb1 = FloatField(validators=[DataRequired()])
+    reb2 = FloatField(validators=[DataRequired()])
